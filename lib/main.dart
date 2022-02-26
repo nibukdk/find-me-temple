a95e1ee3e06078c12c70a5545f603bbe0cbbda2e
nibuk <nibeshkhadka@gmail.com>
2022-02-26 12:08:16 +0545

    DotEnv initialize

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -20,11 +21,14 @@ void main() async {
     SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.black38),
   );
 
+// Initialize firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   FirebaseAuth authInstance = FirebaseAuth.instance;
 
+// Initialize dot env
+  await dotenv.load(fileName: ".env");
   runApp(MyApp(prefs, onBoardCount, authInstance));
 }
